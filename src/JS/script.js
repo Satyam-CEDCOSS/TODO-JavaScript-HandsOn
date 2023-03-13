32ec4ba9d8edcc4b359214d6adc13e9bec5f7b81
Satyam Bajpai <126553444+Satyam-CEDCOSS@users.noreply.github.com>
2023-03-13 17:59:27 +0530

    JavaScript-HandsOn

---
+++ src/JS/script.js
@@ -0,0 +1,149 @@
+// 1 Comment Your JavaScript Code
+
+// This is single line comment 
+/*this is 
+multi line
+comment system */
+
+// 2 Declare JavaScript Variables
+
+var myName;
+
+// 3 Storing Values with the Assignment Operator
+// Setup
+var a;
+// Only change code below this line
+a=7;
+
+// 4 Assigning the Value of One Variable to Another
+// Setup
+var a;
+a = 7;
+var b;
+// Only change code below this line
+b=a;
+
+// 5 Initializing Variables with the Assignment Operator
+var a=9;
+
+// 6 Declare String Variables
+var myFirstName = "Satyam";
+var myLastName = "Bajpai";
+
+// 7 Understanding Uninitialized Variables
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// 8 Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 9 Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 10 Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// 11 Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// 12 Add Two Numbers with JavaScript
+const sum = 10 + 10;
+
+// 13 Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+// 14 Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+// 15 Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+// 16 Increment a Number with JavaScript
+let myVar = 87;
+// Only change code below this line
+myVar++;
+
+// 17 Decrement a Number with JavaScript
+let myVar = 11;
+// Only change code below this line
+myVar--;
+
+// 18 Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+let myDecimal = 6.9
+
+// 19 Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+// 20 Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0; // Change this line
+
+// 21 Finding a Remainder in JavaScript
+const remainder = 2%5;
+
+// 22 Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// 23 Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 24 Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+// 25 Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 26 Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+ 
+// 27 Quoting Strings with Single Quotes
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 28 Escape Sequences in Strings
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// 29 Concatenating Strings with Plus Operator
+const myStr = "This is the start. " + "This is the end."; // Change this line
+ 
+// 30 Concatenating Strings with the Plus Equals Operator
+let myStr= "This is the first sentence.";
+myStr += " This is the second sentence."
